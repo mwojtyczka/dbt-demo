0eaae0d62e744880f76ae4cd0deb50e873dde5fe
Marcin Wojtyczka <marcin.wojtyczka@databricks.com>
2024-10-21 16:27:31 +0200

    update

--- tests/diamonds_check.sql
+++
@@ -1,6 +0,0 @@
--- This season's games happened between 2020-12-12 and 2021-02-06.
--- For this test to pass, this query must return no results.
-
-select date
-from {{ ref('my_python_model') }}
-where carat < 0.2
