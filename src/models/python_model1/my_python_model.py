0654e1906cc3e1692a0c485445b952aa6fb204a0
Marcin Wojtyczka <marcin.wojtyczka@databricks.com>
2024-12-05 18:09:44 +0100

    refactor

--- src/models/python_model1/my_python_model.py
+++
@@ -1,8 +0,0 @@
-# https://docs.getdbt.com/docs/build/python-models
-def model(dbt, session):
-
-    diamonds_df = dbt.ref("diamonds_four_cs")  # you can reference models from different folders
-    final_df = diamonds_df.select("carat", "cut").filter("carat is not NULL")
-    # diamonds_df = session.sql("select carat, cut from diamonds_four_cs where carat is not NULL")
-
-    return final_df
